ffca44ddef557529e59dd0ac9f358f8cce0d02f2
mohammad-sumon <sumon3011@gmail.com>
2022-11-25 13:01:56 +0600

    stripe installed

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 const port = process.env.PORT || 5000;
 
@@ -69,6 +70,22 @@ async function run() {
       const query = { categories: id };
       const selectedCat = await categoryWiseCollection.find(query).toArray();
       res.send(selectedCat);
+    });
+
+    // stripe payment api
+    app.post("/create-payment-intent", async (req, res) => {
+      const booking = req.body;
+      const price = booking.price;
+      const amount = price * 100;
+
+      const paymentIntent = await stripe.paymentIntents.create({
+        currency: "usd",
+        amount: 30000,
+        payment_method_types: ["card"],
+      });
+      res.send({
+        clientSecret: paymentIntent.client_secret,
+      });
     });
 
     // jwt token
